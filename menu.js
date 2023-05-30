4e3ae01257779c66f9871e1756259f791b9401d4
erwin amador <eamador@gmail.com>
2023-05-30 09:43:38 -0600

    Give functionality to hamburger button  in js

---
+++ menu.js
@@ -0,0 +1,9 @@
+const $hamburger = document.querySelector('.hamburger');
+const $menu = document.querySelector('.nav-int');
+const $list = document.querySelector('.ni-list')
+
+$hamburger.addEventListener('click', () => {
+    $hamburger.classList.toggle('active');
+    $menu.classList.toggle('active');
+    $list.classList.toggle('active');
+})
