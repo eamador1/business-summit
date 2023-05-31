074528a68f9792da870340693a0595c91f16ab66
erwin amador <eamador@gmail.com>
2023-05-31 09:26:13 -0600

    Add event DOMContentLoaded

--- menu.js
+++ menu.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
 const $hamburger = document.querySelector('.hamburger');
 const $menu = document.querySelector('.nav-int');
 const $list = document.querySelector('.ni-list')
@@ -6,4 +7,5 @@ $hamburger.addEventListener('click', () => {
     $hamburger.classList.toggle('active');
     $menu.classList.toggle('active');
     $list.classList.toggle('active');
-})
+});
+});
