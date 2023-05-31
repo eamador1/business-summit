c4d1cd0cd68061f146308b5180cad982df113f1b
erwin amador <eamador@gmail.com>
2023-05-31 10:28:40 -0600

    Install and run linters

--- menu.js
+++ menu.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-const $hamburger = document.querySelector('.hamburger');
-const $menu = document.querySelector('.nav-int');
-const $list = document.querySelector('.ni-list')
+  const $hamburger = document.querySelector('.hamburger');
+  const $menu = document.querySelector('.nav-int');
+  const $list = document.querySelector('.ni-list');
 
-$hamburger.addEventListener('click', () => {
+  $hamburger.addEventListener('click', () => {
     $hamburger.classList.toggle('active');
     $menu.classList.toggle('active');
     $list.classList.toggle('active');
-});
+  });
 });
